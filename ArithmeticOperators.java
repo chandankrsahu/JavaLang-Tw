f531f7fe1c666d75a49d493bf819ef673de8667a
Chandan Kumar Sahu <chandansahuji123@gmail.com>
2022-01-10 13:17:49 +0530

    Arithmetic Operations

---
+++ ArithmeticOperators.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ArithmeticOperators{
+
+     public static void main(String[] args) {
+
+        int a=5,b=10;
+        System.out.println(a+b);//add
+        System.out.println(a-b);//subtract
+        System.out.println(a*b);//Multiply
+        System.out.println(a/b);//Divide->gives quotient
+        System.out.println(a%b);//Remainder
+        a++;//post-increment;
+        --b;//pre-decrement;
+        System.out.print(a+" "+b);
+     }
+    }
