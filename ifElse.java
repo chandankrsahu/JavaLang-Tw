def852804123abb5aeff26dd2a45f82c26db4946
Chandan Kumar Sahu <chandansahuji123@gmail.com>
2022-01-10 13:27:43 +0530

    working with if else

---
+++ ifElse.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class ifElse{
+
+     public static void main(String[] args) {
+
+        int n=7;
+        if(n==0){
+            System.out.println("nothing");
+            System.out.print("bye");
+        }
+        else if(n%2==1)
+            System.out.print("odd");
+        else
+            System.out.print("even");
+
+     }
+    }
