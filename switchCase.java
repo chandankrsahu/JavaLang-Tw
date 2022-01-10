05839b938b78294cc67061f21117302122cb0282
Chandan Kumar Sahu <chandansahuji123@gmail.com>
2022-01-10 13:45:00 +0530

    Switch Case Application

---
+++ switchCase.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class switchCase{
+
+     public static void main(String[] args) {
+
+        String st="cks";
+        switch(st){
+            case "abc":
+            System.out.print("abc");
+            break;
+            case "cks":
+            System.out.print("cks");
+            break;
+            default:
+            System.out.print("not found");
+        }
+    }
+}
